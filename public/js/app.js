a42757f8999d07ea8a2d3b56301c456d0f2b882a
Matthew Jamison <matthewjamison@Matthews-MacBook-Pro.local>
2016-04-04 11:44:16 -0400

    adding modals

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,100 @@
 var app = angular.module('versinator', []);
 
 app.controller('VerseController', ['$http', '$scope', function($http, $scope) {
+	var controller = this;
+	controller.newgame = true;
 
-	$scope.lookup = function(verse) {
+	// SET UP THE BOARD TO PLAY A QUESTION
 
+	// GET A RANDOM BOOK AFTER A SHORT DELAY
+		setTimeout(function(){
+			console.log("DELAY OVER")			
+		},1000);
+
+	$scope.question = function() {
+
+	//INITIAL VALUES
+	var randomBook;
+	var randomChapter;
+	var randomVerse;
+	var options = [];
+	controller.newgame = false;
+
+	//FIND A RANDOM NUMBER BETWEEN 0 AND 65 TO DETERMINE BOOK
+	var booknum = Math.floor(Math.random() * 66) + 1;
+
+	//GET THE CORRESPONDING BOOK
+	randomBook = bibleIndex[booknum][0];
+
+	//FIND A RANDOM NUMBER BETWEEN 0 AND MAX CHAPTER IN BOOK
+	randomChapter = Math.floor(Math.random() * (bibleIndex[booknum][1])) + 1;
+	// randomChapter = 1
+
+	//FIND NUMBER OF VERSES IN CHAPTER
+	var maxVerses = bibleIndex[booknum][randomChapter + 1];
+
+	//FIND A RANDOM NUMBER BETWEEN 0 AND MAX VERSE IN CHAPTER
+	randomVerse = Math.floor(Math.random() * maxVerses) + 1;
+
+	// console.log(randomBook + " " + randomChapter + ":" + randomVerse);
+
+	//GET THE TEXT FOR THE VERSE
+	$http({
+		url: '/bible/' + randomBook + '/' + randomChapter + '/' + randomVerse,
+		method: 'GET',
+	}). then(function(response) {
+		controller.text = response.data
+	})
+
+	//RETURNS THE VERSE THAT WAS LOOKED UP
+	controller.answerVerse = randomBook + " " + randomChapter + ":" + randomVerse;
+	controller.book = randomBook;
+	//SETS UP THE SIX OPTIONS
+	//PUSH CORRECT ANSWER TO OPTIONS ARRAY
+	options.push(randomBook);
 	
-		$http({
-			url: '/bible',
-			method: 'GET'
-		}). then(function(response) {
-			console.log('Verse: ', response.data);
-		})
+	//RANDOMIZE INCORRECT BOOK, CHECK THAT IT HASN'T BEEN PICKED YET, AND PUSH IT TO THE ARRAY
+	for (var i = 0; i < 6; i++){
+		var match = false;
+		
+		//GET A NEW RANDOM BOOK
+		var newBook = getRandomBook();
+
+		//IS THE NEW BOOK CURRENTLY IN THE ARRAY?
+		for (var j = 0; j < options.length; j++) {
+			if (newBook === options[j]) {
+				match = true
+			}
+		};
+
+		// IF NOT IN ARRAY ALREADY, PUSH IT TO THE ARRAY
+		if (!match) {
+			options.push(newBook);
+		}
 	};
+
+		//SHUFFLE THE ARRAY
+		shuffle(options);
+
+		//SEND BACK TO INDEX
+		controller.options = options;
+		controller.correct = randomBook;
+	}
+
+	//CHECK IF ANSWER IS RIGHT OR WRONT
+	$scope.evaluate = function(answer) {
+		if (answer === controller.book) {
+			console.log("Right you are!");
+			controller.answer = "Right you are!"
+		} else {
+			console.log("That is not the right answer");
+			controller.answer = "That is not the right answer."
+		}
+		
+	}
+
 }]);
+
+
+
+
--- server.js
+++ server.js
@@ -10,16 +10,51 @@ app.use(express.static('public'));
 //THIS TAKES VERSE REQUEST FROM APP.JS, SENDS IT TO THE SITE, THEN RETURNS IT TO APP.JS
 
 //When /bible route is hit, request data from path
-app.get('/bible', function (req, res) {
-	request('http://www.esvapi.org/v2/rest/passageQuery?key=IP&passage=John+1+1', function (error, response, body) {
+app.get('/bible/:book/:chapter/:verse', function (req, res) {
+
+	//SEND REQUEST TO ESV.ORG
+	request('http://www.esvapi.org/v2/rest/passageQuery?key=cc78384c103e308c&include-footnotes=false&passage=' + (req.params.book) + '+' + (req.params.chapter) + '+' + (req.params.verse), function (error, response, body) {
+		
+		//IF NO ERROR
 		if (!error && response.statusCode == 200) {
+
+			//FIND INDEX OF SPAN TAG THAT INDICATES BEGINNING OF TEXT
 			var begIndex = body.indexOf("</span>")
-			console.log("beginning index = " + begIndex);
-			var endIndex = body.indexOf("(<a");
-			console.log("ending index = " + endIndex);
-			var substring = body.substring((begIndex + 7), endIndex);
-			console.log ("substring = " + substring);
-			res.send(substring);
+
+			//FIND INDEX OF PAREN THAT INDICATES END OF TEXT
+			var endIndex = body.indexOf("(");
+
+			//the verse starting seven characters after the beginning of the span tag
+			var verseText = body.substring((begIndex + 7), endIndex);	
+
+			//CLEAN UP <HTML> TAGS IN TEXT
+			//SET UP AN EMPTY STRING FOR CLEAN TEXT
+			var cleanText = "";
+			var scrub = false
+
+			//LOOP OVER LETTERS IN TEXT		
+			for (var i=0; i<verseText.length; i++) {
+				
+				//beginning of tag or "&" turns scrubber on
+				if (verseText[i] === "<" || verseText[i] === "&") {
+					scrub = true;
+					//add a space
+					cleanText+=" ";
+				};
+
+				//if scrubber is off, add letter to clean text
+				if (!scrub) {
+					cleanText += (verseText[i]);
+				};
+
+				//ending of tag or ";" turns scrubber off
+				if (verseText[i] === ">" || verseText[i] === ";") {
+					scrub = false;
+				};
+			};
+
+			//SEND CLEAN TEXT BACK TO CONTROLLER
+			res.send(cleanText);
 		};
 	});
 });
